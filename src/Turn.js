a514e52a732540b12b9f1b09966968a40db9ad04
Tiala Young <tialayoung@gmail.com>
2023-03-20 20:25:34 -0600

    Add tests and methods for first 3 of 5 requirements

--- src/Turn.js
+++ src/Turn.js
@@ -1,5 +1,20 @@
 class Turn {
+  constructor(guess, currentCard) {
+    this.guess = guess
+    this.currentCard = currentCard
+  };
 
+  returnGuess() {
+    return this.guess;
+  };
+
+  returnCard() {
+    return this.currentCard;
+  };
+
+  evaluateGuess() {
+
+  };
 };
 
 module.exports = Turn;
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,9 +1,44 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Turn = require('..src/Turn');
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', function() {
+  var card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
 
-  
-})
+  beforeEach(function() {
+    // console.log(card);
+  });
+
+  it('should be a function', function() {
+    const turn = new Turn;
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+    const turn = new Turn('someGuess', card);
+    expect(turn).to.be.an.instanceOf(Turn);
+  });
+
+  it('should be able to return player\'s guess', function() {
+    const turn = new Turn('someGuess', card);
+    expect(turn.returnGuess()).to.equal('someGuess');
+  });
+
+  it('should be able to return the current card', function() {
+    const turn = new Turn('someGuess', card);
+    expect(turn.returnCard()).to.equal(card);
+    expect(turn.returnCard()).to.deep.equal({
+      id: 1,
+      question: "What is Robbie's favorite animal",
+      answers: [ 'sea otter', 'pug', 'capybara' ],
+      correctAnswer: 'sea otter'
+    });
+  });
+
+  it('should validate if player guess matches correct answer for current card', function() {
+    const turn = new Turn('someGuess', card);
+    // TO DO: finish this test
+  });
+});
