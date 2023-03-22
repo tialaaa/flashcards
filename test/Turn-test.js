0f60ee6bed6e857d93b014f0c363ca1c2ac8ec19
Tiala Young <tialayoung@gmail.com>
2023-03-22 17:10:40 -0600

    Refactor for more DRY and more robust test of new instance

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,28 +5,30 @@ const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
 describe('Turn', function() {
-  var card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-
-  it('should be a function', function() {
-    const turn = new Turn;
-    expect(Turn).to.be.a('function');
+  let card;
+  let turn1;
+  
+  beforeEach(function() {
+    card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    turn1 = new Turn('pug', card);
   });
 
-  it('should be an instance of Turn', function() {
-    const turn = new Turn('someGuess', card);
-    expect(turn).to.be.an.instanceOf(Turn);
+  it('should be a function that creates an instance of Turn', function() {
+    expect(turn1).to.be.an.instanceOf(Turn);
+  });
+
+  it('should be instantiated with two arguments: a string for the guess, and a Card object for the current card', function() {
+    expect(turn1.guess).to.be.a('string');
+    expect(turn1.currentCard).to.be.an('object');
   });
 
   it('should be able to return player\'s guess', function() {
-    const turn = new Turn('someGuess', card);
-    expect(turn.returnGuess()).to.equal('someGuess');
+    expect(turn1.returnGuess()).to.equal('pug');
   });
 
   it('should be able to return the current card', function() {
-    const turn = new Turn('someGuess', card);
-
-    expect(turn.returnCard()).to.equal(card);
-    expect(turn.returnCard()).to.deep.equal({
+    expect(turn1.returnCard()).to.equal(card);
+    expect(turn1.returnCard()).to.deep.equal({
       id: 1,
       question: "What is Robbie's favorite animal",
       answers: [ 'sea otter', 'pug', 'capybara' ],
@@ -34,19 +36,17 @@ describe('Turn', function() {
     });
   });
 
-  it('should validate if player guess matches correct answer for current card', function() {
-    const turn1 = new Turn('capybara', card);
-    const turn2 = new Turn('sea otter', card);
-    
+  it('should validate if player\'s guess matches the correct answer of current card', function() {
     expect(turn1.evaluateGuess()).to.equal(false);
+
+    const turn2 = new Turn('sea otter', card);
     expect(turn2.evaluateGuess()).to.equal(true);
   });
 
-  it('should give feedback on whether the guess is correct or not', function() {
-    const turnA = new Turn('capybara', card);
-    const turnB = new Turn('sea otter', card);
-
-    expect(turnA.giveFeedback()).to.equal('incorrect!');
-    expect(turnB.giveFeedback()).to.equal('correct!');
-  })
+  it('should return a feedback message telling if the guess is correct or not', function() {
+    expect(turn1.giveFeedback()).to.equal('incorrect!');
+    
+    const turn2 = new Turn('sea otter', card);
+    expect(turn2.giveFeedback()).to.equal('correct!');
+  });
 });
