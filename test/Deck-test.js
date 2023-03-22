9ff15ae0f79f0ab95c119bfcb95e42da32f52c1c
Tiala Young <tialayoung@gmail.com>
2023-03-22 17:09:38 -0600

    Make test of new instance more robust

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -15,7 +15,7 @@ describe('Deck', function() {
     card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
   });
 
-  // Alternate version to ask about:
+  // Alternate version to ask about - not using beforeEach:
   // const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
   // const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
   // const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
@@ -24,8 +24,13 @@ describe('Deck', function() {
     const deck = new Deck([card1, card2, card3]);
 
     expect(deck.list[0]).to.equal(card1);
+    expect(deck.list[0]).to.be.an('object');
+
     expect(deck.list[1]).to.equal(card2);
+    expect(deck.list[1]).to.be.an('object');
+
     expect(deck.list[2]).to.equal(card3);
+    expect(deck.list[2]).to.be.an('object');
   });
 
   it('should know how many cards are in the deck', function() {
