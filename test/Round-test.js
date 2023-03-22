3169c1db704872c8609442fcd9f767acc25b555c
Tiala Young <tialayoung@gmail.com>
2023-03-22 15:16:36 -0600

    Finish testing Round class

--- test/Round-test.js
+++ test/Round-test.js
@@ -23,10 +23,13 @@ describe('Round', function() {
 
   it('should begin with current card as the first card in the deck', function() {
     expect(round.currentCard).to.equal(card1);
-  })
+  });
 
   it('should be able to return the current card', function() {
     expect(round.returnCurrentCard()).to.equal(card1);
+
+    round.currentCard = card3;
+    expect(round.returnCurrentCard()).to.equal(card3);
   });
 
   it('should create a new instance of Turn when player takes a turn', function() {
@@ -37,8 +40,8 @@ describe('Round', function() {
   it('should increase the turns count, regardless of whether the guess is correct or incorrect', function() {
     expect(round.turns).to.equal(0);
 
-    round.takeTurn('pug');
     round.takeTurn('sea otter');
+    round.takeTurn('liver');
     expect(round.turns).to.equal(2);
   });
 
@@ -52,19 +55,45 @@ describe('Round', function() {
     expect(round.returnCurrentCard()).to.equal(card3);
   });
 
-  it('should store the card ID of incorrect guesses in an array', function() {
+  it('should store the card ID of only incorrect guesses in an array', function() {
     expect(round.incorrectGuesses).to.be.empty;
 
     round.takeTurn('donkey');
     expect(round.incorrectGuesses[0]).to.equal(card1.id);
+    expect(round.incorrectGuesses[0]).to.equal(1);
 
     round.takeTurn('gallbladder');
     expect(round.incorrectGuesses).to.have.lengthOf(1);
   });
 
   it('should return feedback regarding whether the guess is incorrect or correct', function() {
-    expect(round.takeTurn('sea otter')).to.equal('Correct!');
+    expect(round.takeTurn('sea otter')).to.equal('correct!');
 
-    expect(round.takeTurn('heart')).to.equal('Incorrect!');
+    expect(round.takeTurn('heart')).to.equal('incorrect!');
+  });
+
+  it('should calculate and return the percentage of correct guesses', function() {
+    expect(round.calculatePercentCorrect()).to.be.NaN;
+
+    round.takeTurn('sea otter');
+    expect(round.calculatePercentCorrect()).to.equal(100);
+
+    round.takeTurn('lung');
+    round.takeTurn('Fitzgerald');
+    expect(round.calculatePercentCorrect()).to.equal(67);
+  });
+
+  it('should return a message displaying the final score when the round ends', function() {
+    round.takeTurn('walrus');
+    round.takeTurn('gallbladder');
+    round.takeTurn('Bob');
+    expect(round.endRound()).to.equal('** Round over! ** You answered 33% of the questions correctly!');
+  });
+
+  it('should always display the correct final score when the round ends', function() {
+    round.takeTurn('sea otter');
+    round.takeTurn('gallbladder');
+    round.takeTurn('Fitzgerald');
+    expect(round.endRound()).to.equal('** Round over! ** You answered 100% of the questions correctly!');
   });
 });
